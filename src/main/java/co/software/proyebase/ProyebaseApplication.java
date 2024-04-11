b4fb40ed2c1b449332977506e02fdd5f95030ce1
ACCP <acorchuelop@ucentral.edu.co>
2024-04-10 23:10:49 -0500

    Commit inicial

---
+++ src/main/java/co/software/proyebase/ProyebaseApplication.java
@@ -0,0 +1,13 @@
+package co.software.proyebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/software/proyebase/ProyebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package co.software.proyebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
