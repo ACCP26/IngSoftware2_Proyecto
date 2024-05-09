e9d769463e08487dc352d180a22587744b9fae18
ACCP <acorchuelop@ucentral.edu.co>
2024-05-09 16:20:35 -0500

    Ajuste de CROS para la conexión con parte front

--- src/main/java/co/software/proyebase/ProyebaseApplication.java
+++ src/main/java/co/software/proyebase/ProyebaseApplication.java
@@ -2,14 +2,11 @@ package co.software.proyebase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import co.software.proyebase.entidades.Estudiantes;
-import co.software.proyebase.repositorios.RepositorioEstudiantes;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.log4j.Log4j2;
 
-import java.util.List;
 
 @SpringBootApplication
+@Log4j2
 public class ProyebaseApplication{
 
 	public static void main(String[] args) {
@@ -17,25 +14,5 @@ public class ProyebaseApplication{
 		SpringApplication.run(ProyebaseApplication.class, args);
 		System.out.println("Proyecto software");
 	}
-/*
-	@Autowired
-	RepositorioEstudiantes repoEstudiante;
-	@Override
-	public void run(String... args) throws Exception{
 
-
-		Estudiantes e = new Estudiantes();
-		e.setNombre("Andres");
-		e.setApellido("Corchuelo");
-		e.setIdentificacion(1014736717);
-
-		repoEstudiante.save(e);
-
-		List<Estudiantes> listEstu = (List<Estudiantes>) repoEstudiante.findAll();
-		for(Estudiantes estudiantes : listEstu){
-			System.out.println(estudiantes);
-		}
-	}
-
-*/
 }
--- src/main/java/co/software/proyebase/controladores/ControladorEstudiantes.java
+++ src/main/java/co/software/proyebase/controladores/ControladorEstudiantes.java
@@ -1,23 +1,18 @@
 package co.software.proyebase.controladores;
 
-import co.software.proyebase.entidades.Estudiantes;
 import co.software.proyebase.dto.EstudianteDto;
 import co.software.proyebase.servicios.ServicioEstudiantes;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/estudiantes")
-@CrossOrigin(origins = "${spring.webmvc.cors.allowed-origins)",
-        methods = {RequestMethod.POST})
+@CrossOrigin(origins = "${spring.webmvc.cors.allowed-origins}",
+        methods = {RequestMethod.GET, RequestMethod.DELETE,RequestMethod.POST,RequestMethod.PUT})
 
 public class ControladorEstudiantes {
 
@@ -25,7 +20,7 @@ public class ControladorEstudiantes {
 
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<EstudianteDto> registrar(@Validated @RequestBody EstudianteDto entidadDto){
+    public ResponseEntity<EstudianteDto>registrar(@Validated @RequestBody EstudianteDto entidadDto){
         entidadDto = servicioEstudiante.registrar(entidadDto);
 
         return new ResponseEntity<>(entidadDto, HttpStatus.CREATED);
