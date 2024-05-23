43a298b0e97d6fd1e7303a7184b0ca39f847a724
ACCP <acorchuelop@ucentral.edu.co>
2024-05-23 15:43:59 -0500

    Adicion pruebas unitarias para el repositorio de asignaturas.

--- src/test/java/co/software/proyebase/repositorios/RepositorioAsignaturasTest.java
+++ src/test/java/co/software/proyebase/repositorios/RepositorioAsignaturasTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,5 +36,38 @@ class RepositorioAsignaturasTest {
 
         assertThat(comprobar.isPresent()).isTrue();
         assertThat(comprobar.get().getNombre()).isEqualTo("Ingenieria de software");
+    }
+
+    @Test
+    public void listarSolicitud(){
+
+        Asignatura asignatura = TestData.crearAsignatura();
+
+        List<Asignatura> asignaturaList = (List<Asignatura>) repositorioAsignaturas.findAll();
+        assertThat(asignaturaList).isNotNull();
+        assertThat(asignaturaList.size()).isGreaterThan(0);
+        assertThat(asignaturaList).contains(asignatura);
+    }
+
+    @Test
+    public void actualizarSolicitud(){
+
+        Asignatura asignatura = TestData.crearAsignatura();
+        Asignatura guardar = repositorioAsignaturas.save(asignatura);
+
+        Optional<Asignatura> actualizar = repositorioAsignaturas.findById(guardar.getCodigo());
+        assertThat(actualizar.isPresent()).isTrue();
+
+        Asignatura asignaturaActualizar = actualizar.get();
+        asignaturaActualizar.setNombre("Analisis de algoritmos");
+        asignaturaActualizar.setPeriodo("2023-2");
+        repositorioAsignaturas.save(asignaturaActualizar);
+
+        Optional<Asignatura> actualizada = repositorioAsignaturas.findById(guardar.getCodigo());
+        assertThat(actualizada.isPresent()).isTrue();
+
+        Asignatura asignatura1 = actualizada.get();
+        assertThat(asignatura1.getNombre()).isEqualTo("Analisis de algoritmos");
+        assertThat(asignatura1.getPeriodo()).isEqualTo("2023-2");
     }
 }
