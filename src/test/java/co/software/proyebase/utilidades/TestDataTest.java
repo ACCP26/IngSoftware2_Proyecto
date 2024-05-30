6aa06b8914616fb7b63937ee864828e7e867f749
NatalyVar <nvargas@ucentral.edu.co>
2024-05-30 02:01:26 -0500

    creacion de la rama HU04, ControladorNota, nota, ServicioNota, RepositorioNota y los test

---
+++ src/test/java/co/software/proyebase/utilidades/TestDataTest.java
@@ -0,0 +1,20 @@
+package co.software.proyebase.utilidades;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestDataTest {
+
+    @Test
+    void crearAsignatura() {
+    }
+
+    @Test
+    void crearEstudiantes() {
+    }
+
+    @Test
+    void crearNota() {
+    }
+}
